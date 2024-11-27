897ebcaa9ecdd720d6d4e68d856fb99b113e65b5
Stanley Garrett <onlycallmestanley@gmail.com>
2024-11-27 00:37:06 -0500

    project 4 stage 1update #1

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+const cardData1 = {
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+
+const cardData2 = {
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
+
+const cardData3 = {
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
+
+const cardData4 = {
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
+
+const cardData5 = {
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
+
+const cardData6 = {
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
+
+const initialCards = [
+  cardData1,
+  cardData2,
+  cardData3,
+  cardData4,
+  cardData5,
+  cardData6,
+];
+
+console.log(initialCards);
