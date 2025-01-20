ed7d02f0bbaed213c4c67046c7daa3c687e9230d
Stanley Garrett <onlycallmestanley@gmail.com>
2025-01-19 23:56:36 -0500

    project 4 stage 3 TheDom#1

--- scripts/index.js
+++ scripts/index.js
@@ -38,3 +38,14 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const modalCloseButton = document.querySelector("#modal-close-button");
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
