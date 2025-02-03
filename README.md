2913821cf1804664140818871b720659308a7eb7
Stanley Garrett <onlycallmestanley@gmail.com>
2025-02-02 23:45:14 -0500

    project 4 final stage Javascript  update #1

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Project 3: Around The U.S.
+# Project 4: Around The U.S.
 
 ### Overview  
 
-This is project 3. An interactive webpage showcasing cards of various locations utilizing grid, flex box, BEM, prettier, and so much more. This webpage was designed to be responsive across mobile, tablet and desktop devices with the use of media queries at specific demensions whenever there are any signs of change in display. A few things I would love to improve on would be; utilizing command line more when navigating/setting up my project, understanding grid, and getting more tricks on flexbox.
+This is project 4. An interactive webpage showcasing cards of various locations utilizing Html/Css, Javascript, grid, flex box, BEM, prettier, and so much more. This webpage was designed to be responsive across mobile, tablet and desktop devices with the use of media queries at specific demensions whenever there are any signs of change in display. A few things I would love to improve on would be; utilizing command line more when navigating/setting up my project, understanding grid, and getting more tricks on flexbox.
   
 **Video**  
   
-* [Project 3 Video Presentation](https://drive.google.com/file/d/1BLstGCrfW-CNTc3Z9n0KmazFGhg__CLa/view?usp=sharing)  
+* [Project 4 Video Presentation](https://drive.google.com/file/d/1BLstGCrfW-CNTc3Z9n0KmazFGhg__CLa/view?usp=sharing)  
 
-**GitHub Pages Project 3 Link**
+**GitHub Pages Project 4 Link**
 
-* [Project 3 Link](https://stanleyluxx.github.io/se_project_aroundtheus/)
+* [Project 4 Link](https://stanleyluxx.github.io/se_project_aroundtheus/)
   
 **Images**  
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,104 @@
-const cardData1 = {
-  name: "Yosemite Valley",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-};
-
-const cardData2 = {
-  name: "Lake Louise",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-};
-
-const cardData3 = {
-  name: "Bald Mountains",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-};
-
-const cardData4 = {
-  name: "Latemar",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-};
-
-const cardData5 = {
-  name: "Vanoise National Park",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-};
-
-const cardData6 = {
-  name: "Lago di Braies",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-};
+/*------------------*/
+/*   Cards          */
+/*------------------*/
 
 const initialCards = [
-  cardData1,
-  cardData2,
-  cardData3,
-  cardData4,
-  cardData5,
-  cardData6,
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
 
-console.log(initialCards);
+/*------------------*/
+/*    Elements      */
+/*------------------*/
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = document.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template");
+
+/*------------------*/
+/*   Functions      */
+/*------------------*/
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(initialCards) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardTitleEl.textContent = initialCards.name;
+  cardImageEl.src = initialCards.link;
+  cardImageEl.alt = initialCards.name;
+  return cardElement;
+}
+
+/*------------------*/
+/*  Event Handlers  */
+/*------------------*/
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/*------------------*/
+/* Event Listeners  */
+/*------------------*/
+
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
 const modalCloseButton = document.querySelector("#modal-close-button");
 modalCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+  closePopup();
+});
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
 });
