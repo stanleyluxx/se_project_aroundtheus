fb5fb1c263b74870e5b6681fbc6447ddf3537a3c
Stanley Garrett <onlycallmestanley@gmail.com>
2025-09-30 15:07:17 -0400

    Saving button feature and html avatar button formatting fix

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,6 +6,8 @@ class PopupWithForm extends Popup {
     this._handleFormSubmit = handleFormSubmit;
     this._form = this._popup.querySelector(".modal__form");
     this._inputList = this._form.querySelectorAll(".modal__input");
+    this._submitButton = this._form.querySelector(".modal__button");
+    this._submitButtonText = this._submitButton.textContent;
   }
   // show loading state
   renderLoading(isLoading, loadingText = "Saving...") {
@@ -15,7 +17,7 @@ class PopupWithForm extends Popup {
       this._submitButton.textContent = this._submitButtonText;
     }
   }
-  
+
   _getInputValues() {
     const formValues = {};
     this._inputList.forEach((input) => {
